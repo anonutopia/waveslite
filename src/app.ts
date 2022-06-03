c85207f59e525228a11dc47f708ac29fbe4c1d6f
Pragmatic <cryptopragmatic@protonmail.com>
2022-06-04 00:28:31 +0200

    wallet - fix send

--- src/app.ts
+++ src/app.ts
@@ -193,7 +193,7 @@ class Wallet {
     }
 
     updateAmount() {
-        var currency = $("#sendCurrency").val();
+        var currency = '';
 
         var amount = 0;
         var dp = this.getDecimalPlaces(String(currency));
@@ -225,39 +225,39 @@ class Wallet {
         $("#amount").val(String(balance.toFixed(decimalPlaces)));
     }
 
-    updateFeeAmount() {
-        var currency = $("#sendCurrency").val();
-        var dp = this.getDecimalPlaces(String(currency));
-        var decimalPlaces = 0;
-        if (currency == AHRK) {
-            $("#feeAsset").html("AHRK");
-            decimalPlaces = 6;
-        } else if (currency == AEUR) {
-            $("#feeAsset").html("AEUR");
-            decimalPlaces = 2;
-        } else if (currency == "") {
-            $("#feeAsset").html("WAVES");
-            decimalPlaces = 8;
-        } else if (currency == AINT) {
-            if (t.lang == "hr") {
-                $("#feeAsset").html("AHRK");
-                decimalPlaces = 6;
-                dp = this.getDecimalPlaces(AHRK);
-            } else if (t.lang == "en") {
-                $("#feeAsset").html("AEUR");
-                decimalPlaces = 2;
-                dp = this.getDecimalPlaces(AEUR);
-            }
-        } else if (currency == ANOTE) {
-            $("#feeAsset").html("ANOTE");
-            decimalPlaces = 8;
-        }
+    // updateFeeAmount() {
+    //     var currency = $("#sendCurrency").val();
+    //     var dp = this.getDecimalPlaces(String(currency));
+    //     var decimalPlaces = 0;
+    //     if (currency == AHRK) {
+    //         $("#feeAsset").html("AHRK");
+    //         decimalPlaces = 6;
+    //     } else if (currency == AEUR) {
+    //         $("#feeAsset").html("AEUR");
+    //         decimalPlaces = 2;
+    //     } else if (currency == "") {
+    //         $("#feeAsset").html("WAVES");
+    //         decimalPlaces = 8;
+    //     } else if (currency == AINT) {
+    //         if (t.lang == "hr") {
+    //             $("#feeAsset").html("AHRK");
+    //             decimalPlaces = 6;
+    //             dp = this.getDecimalPlaces(AHRK);
+    //         } else if (t.lang == "en") {
+    //             $("#feeAsset").html("AEUR");
+    //             decimalPlaces = 2;
+    //             dp = this.getDecimalPlaces(AEUR);
+    //         }
+    //     } else if (currency == ANOTE) {
+    //         $("#feeAsset").html("ANOTE");
+    //         decimalPlaces = 8;
+    //     }
 
-        var fee = this.getFee(String(currency));
-        var feeStr = fee / dp;
+    //     var fee = this.getFee(String(currency));
+    //     var feeStr = fee / dp;
 
-        $("#feePrice").html(String(feeStr.toFixed(decimalPlaces)));
-    }
+    //     $("#feePrice").html(String(feeStr.toFixed(decimalPlaces)));
+    // }
 
     updateAmountExchange() {
         var currency = $("#fromCurrency").val();
@@ -402,7 +402,7 @@ class Wallet {
     }
 
     async send() {
-        var currency = $("#sendCurrency").val();
+        var currency = '';
         var decimalPlaces = this.getDecimalPlaces(String(currency));
         var fee = this.getFee(String(currency));
         var feeCurrency = currency;
@@ -964,10 +964,10 @@ $("#buttonCollect").on( "click", function() {
     wallet.collectEarnings(AHRKADDRESS);
 });
 
-$("#sendCurrency").on( "change", function() {
-    wallet.updateAmount();
-    wallet.updateFeeAmount();
-});
+// $("#sendCurrency").on( "change", function() {
+//     wallet.updateAmount();
+//     wallet.updateFeeAmount();
+// });
 
 $("#fromCurrency").on( "change", function() {
     wallet.updateAmountExchange();
